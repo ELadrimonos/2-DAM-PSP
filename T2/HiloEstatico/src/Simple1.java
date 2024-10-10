d831a1acd491732e96b0ae00869dd2b3a9284c47
Adrián Primo <adrpriber@alu.edu.gva.es>
2024-10-10 10:05:43 +0200

    :adhesive_bandage: Actualizado Simple1

--- T2/HiloEstatico/src/Simple1.java
+++ T2/HiloEstatico/src/Simple1.java
@@ -1,10 +1,19 @@
+import java.util.ArrayList;
+
 public class Simple1 {
     public static void main(String[] args) {
+
+        ArrayList<Hilo> hilos = new ArrayList<>();
+
         for (int i = 0; i < 5; i++) {
-            Hilo hilo = new Hilo();
+            hilos.add(new Hilo());
+        }
+
+        for (Hilo hilo : hilos) {
             hilo.start();
         }
-        // Si no espero que finalicen todos los hilos, el valor parece aleatorio
+
+        // Si no espero que finalicen todos los hilos (con join), el valor parece aleatorio
         System.out.println(Hilo.contador);
     }
 }
