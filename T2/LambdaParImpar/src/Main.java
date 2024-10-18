6eb2c14e8d1f7bcea4ec81fd7db6371a25583720
Adrián Primo <adrpriber@alu.edu.gva.es>
2024-10-18 08:10:30 +0200

    :triangular_flag_on_post: Cambiado formato mostrar Pares e Impares

--- T2/LambdaParImpar/src/Main.java
+++ T2/LambdaParImpar/src/Main.java
@@ -2,19 +2,22 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         Thread hiloPares = new Thread(() -> {
             for (int i = 2; i < 1000; i += 2) {
-                System.out.println(i);
+                System.out.print(i + ", ");
             }
+            System.out.println();
         });
 
         hiloPares.start();
 
         // Así puedo comprobar que vayan bien los impares
         hiloPares.join();
+        System.out.println();
 
         Thread hiloImpares = new Thread(() -> {
             for (int i = 1; i < 1000; i += 2) {
-                System.out.println(i);
+                System.out.print(i + ", ");
             }
+            System.out.println();
         });
         hiloImpares.start();
 
